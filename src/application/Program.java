32623d788399950d19344c44d87b19e294eedd76
Matheus Rocha <theucsrocha@gmail.com>
2025-09-05 11:18:30 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,6 +30,19 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		 
+		
+		System.out.println("====TEST INSERT====");
+		
+		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	}
